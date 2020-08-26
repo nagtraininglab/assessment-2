e57a83786a71d4c22f98b3cff91609ba427604d3
nlin3696 <38114559+nlin3696@users.noreply.github.com>
2020-08-26 10:53:41 -0400

    test fix

--- Noah-Lin/MoneyTxrServiceApplicationTests.java
+++ Noah-Lin/MoneyTxrServiceApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+inport com.example.demo.model.TxrServiceImpl;
+
 
 
 @SpringBootTest
